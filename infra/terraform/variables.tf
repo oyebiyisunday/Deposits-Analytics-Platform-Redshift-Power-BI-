da0a48b36f9c2da6c965fd1eb5df4fb8e6eca44b
Sunday Oyebiyi <oyebiyisunday@gmail.com>
2025-10-13 16:15:48 -0400

    Updated files

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -5,3 +5,18 @@ variable "private_subnets" { type=list(string) default=["10.42.1.0/24","10.42.2.
 variable "public_subnets"  { type=list(string) default=["10.42.11.0/24","10.42.12.0/24"] }
 variable "redshift_username" { type=string }
 variable "redshift_password" { type=string sensitive=true }
+
+# Matillion Integration Variables
+variable "matillion_base_url" { 
+  type=string 
+  description="Base URL for Matillion ETL instance"
+}
+variable "matillion_user" { 
+  type=string 
+  description="Matillion ETL username"
+}
+variable "matillion_password" { 
+  type=string 
+  sensitive=true
+  description="Matillion ETL password"
+}
